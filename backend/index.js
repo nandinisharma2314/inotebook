b1311fac2779b56038907fea3d9e6b2a5887f3ce
nandinisharma2314 <nandinisharma142004@gmail.com>
2026-05-26 12:34:14 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,21 @@
+require("dotenv").config();
+
 const connectToMongo = require("./db");
 const express = require("express");
-var cors = require('cors');
-
+const cors = require("cors");
 
 connectToMongo();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
-//Middlewares
+
 app.use(express.json());
 
-
-//Available routes
+// Available routes
 app.use("/api/auth", require("./routes/auth"));
-app.use('/api/notes', require('./routes/notes'))
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
   console.log(`iNotebook backend listening on port ${port}`);
